156077ce197794b3d11d25e485f3e525bccd59c4
Yifan Zhu <yifanzhu95@gmail.com>
2023-08-24 13:19:26 -0400

    Updated example

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from franka-python-controller import FrankaController
+from franka_python_controller import FrankaController
 from klampt import RobotModel, vis
 from klampt import WorldModel,RobotModel
 from klampt.model import ik,collide
@@ -16,34 +16,34 @@ params = {'address': ''} ## TBD, joint stiffness can also be set here
 
 # controller instance
 controller = FrankaController(name = 'Franka', robot_model = robot_model, EE_link = EE_link, \
-    collision_checker = collision_checker, params)
+    collision_checker = collision_checker, params = params)
 
-# start robot
-controller.initialize()
-controller.start() 
+# # start robot
+# controller.initialize()
+# controller.start() 
 
-# get current states
-current_config = controller.get_joint_config()
-current_joint_velocity = controller.get_joint_velocity()
-current_joint_torques = controller.get_joint_torques()
+# # get current states
+# current_config = controller.get_joint_config()
+# current_joint_velocity = controller.get_joint_velocity()
+# current_joint_torques = controller.get_joint_torques()
 
-current_EE_transform = controller.get_EE_transform(tool_center = se3.identity()) #transform of tool center, 
-    #specified in the tool frame (last frame of the robot)
-current_EE_velocity = controller.get_EE_velocity()
-current_EE_wrench = controller.get_EE_wrench() # This is in the world frame but can be easily transformed with
-    # the current_EE_transform
+# current_EE_transform = controller.get_EE_transform(tool_center = se3.identity()) #transform of tool center, 
+#     #specified in the tool frame (last frame of the robot)
+# current_EE_velocity = controller.get_EE_velocity()
+# current_EE_wrench = controller.get_EE_wrench() # This is in the world frame but can be easily transformed with
+#     # the current_EE_transform
 
 
 
-# control the robot
-controller.set_joint_config(current_config)
-time.sleep(1)
-controller.set_EE_transform(current_EE_transform)
-time.sleep(1)
-controller.set_EE_velocity([0]*6)
-time.sleep(1)
+# # control the robot
+# controller.set_joint_config(current_config)
+# time.sleep(1)
+# controller.set_EE_transform(current_EE_transform)
+# time.sleep(1)
+# controller.set_EE_velocity([0]*6)
+# time.sleep(1)
 
-# shutdown the robot
-controller.close()
+# # shutdown the robot
+# controller.close()
 
 
--- franka-python-controller/__init__.py
+++
@@ -1,1 +0,0 @@
-from franka_controller import FrankaController
